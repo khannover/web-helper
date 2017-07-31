36977535030de28a3e667675c07443b20561d151
Kai <kai@hannover38.de>
2017-07-31 15:43:45 +0200

    Update qr.php

--- qr.php
+++ qr.php
@@ -2,16 +2,21 @@
 
 $url = $_GET['url'];
 $id = hash("sha256", $url);
+$qrcache = "qrcodes";
 
-if(!file_exists("qrcodes/$id.png")){
-     exec("qrencode " . escapeshellarg($url) . " -o qrcodes/" . $id . ".png -t png");
+if (!mkdir($qrcache, 0755, true)) {
+    die('Could not create cache directory ' . $qrcache);
+}
+
+if(!file_exists("$qrcache/$id.png")){
+     exec("qrencode " . escapeshellarg($url) . " -o $qrcache/" . $id . ".png -t png");
 }
 
 header('Content-type: image/png');
-header('Content-Length: ' . filesize("qrcodes/$id.png"));
-$fp = fopen("qrcodes/$id.png", "rb");
+header('Content-Length: ' . filesize("$qrcache/$id.png"));
+$fp = fopen("$qrcache/$id.png", "rb");
 fpassthru($fp);
 
 
 /*  uncomment the next line if you don't want to store the qr images in your filesystem (may be slower) */
-//unlink("qrcodes/$id.png");
+//unlink("$qrcache/$id.png");
